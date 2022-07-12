d9f525971f013717b72e3dad32932fe2da5d9116
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2022-07-11 23:41:22 -0400

    home delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-      <h1>Crypto Verse!</h1>
+    <div clasName='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'></div>
+      <div className='footer'></div>
     </div>
   );
 };
